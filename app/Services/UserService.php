a669e12687de5524d5d50a9bd48c1770120be60f
mancioks <mantas.kryzevicius@gmail.com>
2022-10-15 18:04:29 +0300

    VNVD-2 UserService getUserPopularProducts

---
+++ app/Services/UserService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\OrderItem;
+use App\Models\Product;
+use App\Models\Status;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class UserService
+{
+    public static function getUserPopularProducts(User $user, int $limit = 10)
+    {
+        $orders = $user->orders()->whereNotIn('status_id', [Status::CANCELED, Status::DECLINED]);
+
+        $ordersId = $orders->pluck('id')->toArray();
+
+        $items = OrderItem::query()->whereIn('order_id', $ordersId)->get();
+
+        $userItems = [];
+        foreach ($items as $item) {
+            if (Product::query()->where(['id' => $item->product_id])->exists()) {
+                if (!array_key_exists($item->product_id, $userItems)) {
+                    $userItems[$item->product_id] = 0;
+                }
+                $userItems[$item->product_id] += $item->qty;
+            }
+        }
+
+        arsort($userItems);
+
+        $productIds = array_keys(array_slice($userItems, 0, $limit, true));
+        $productOrderIds = implode(',', $productIds);
+
+        $products = Product::query()->whereIn('id', $productIds)->orderByRaw(DB::raw("FIELD(id, $productOrderIds)"))->get();
+
+        return $products;
+    }
+}
